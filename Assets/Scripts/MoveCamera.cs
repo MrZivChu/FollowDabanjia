67b1bebe3e6976b7f961f522a0cb543288e204ba
zhuwenhai <454719889@qq.com>
2017-06-01 09:37:03 +0800

    first commit

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+    Transform myTransform;
+    Camera myCamera;
+    private void Start()
+    {
+        myTransform = transform;
+        myCamera = GetComponent<Camera>();
+    }
+
+    public void Up()
+    {
+
+    }
+
+    public void Down()
+    {
+
+    }
+
+    public void Left()
+    {
+
+    }
+
+    public void Right()
+    {
+
+    }
+
+    public void Near()
+    {
+
+    }
+
+    public void Far()
+    {
+
+    }
+
+
+
+
+}
