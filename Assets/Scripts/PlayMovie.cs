3456988afa0520b8befeb141b02d07e176cb2309
zhuwenhai <454719889@qq.com>
2017-06-24 17:18:10 +0800

    add

---
+++ Assets/Scripts/PlayMovie.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayMovie : MonoBehaviour
+{
+    public MovieTexture movTexture;
+    Image imgae = null;
+    Renderer theRenderer = null;
+
+    Texture orignalTexture1;
+    Texture orignalTexture2;
+
+    void Start()
+    {
+        imgae = GetComponent<Image>();
+        if (imgae != null)
+        {
+            orignalTexture1 = imgae.material.mainTexture;
+            imgae.material.mainTexture = movTexture;
+        }
+        else
+        {
+            theRenderer = GetComponent<Renderer>();
+            if (theRenderer != null)
+            {
+                orignalTexture2 = theRenderer.material.mainTexture;
+                theRenderer.material.mainTexture = movTexture;
+            }
+        }
+        movTexture.loop = true;
+        movTexture.Play();
+    }
+
+    private void OnDestroy()
+    {
+        if (imgae != null)
+        {
+            imgae.material.mainTexture = orignalTexture1;
+        }
+        if (theRenderer != null)
+        {
+            theRenderer.material.mainTexture = orignalTexture2;
+        }
+    }
+}
---
+++ Assets/Scripts/SelectRootRoom.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SelectRootRoom : MonoBehaviour
+{
+    public SelectRoom selectRoom;
+    public GameObject main2;
+
+    public Button Btn1;
+    public Button Btn2;
+    public Button Btn3;
+
+    private void Start()
+    {
+        EventTriggerListener.Get(Btn1.gameObject, 0).onClick = MyClick;
+        EventTriggerListener.Get(Btn2.gameObject, 1).onClick = MyClick;
+        EventTriggerListener.Get(Btn3.gameObject, 2).onClick = MyClick;
+    }
+
+    void MyClick(GameObject go, object param)
+    {
+        int type = (int)param;
+        selectRoom.selectToggleIndex = type;
+        main2.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+}
