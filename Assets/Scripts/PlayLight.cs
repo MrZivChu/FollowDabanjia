5a6433a59c5dbdb15d94c5f6b5d03f23f2e38b57
zhuwenhai <454719889@qq.com>
2017-06-26 00:33:51 +0800

    update

--- Assets/Scripts/PlayLight.cs
+++ Assets/Scripts/PlayLight.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 public class PlayLight : MonoBehaviour {
 
     public GameObject lightObj;
-    public GameObject target;
+
+    public GameObject baiLight;
+    public GameObject heiLight;
 
     [HideInInspector]
     public bool isLigt = false;
@@ -19,7 +21,8 @@ public class PlayLight : MonoBehaviour {
         if (lightObj) {
             isLigt = true;
             lightObj.SetActive(true);
-            Utils.ChangeShaderEmission(target, new Color(0.90f, 0.86f, 0.61f, 1f));
+            baiLight.SetActive(true);
+            heiLight.SetActive(false);
         }
     }
 
@@ -27,13 +30,8 @@ public class PlayLight : MonoBehaviour {
         if (lightObj) {
             isLigt = false;
             lightObj.SetActive(false);
-            Utils.ChangeShaderEmission(target, Color.red);
-        }
-    }
-
-    private void OnDestroy() {
-        if (lightObj) {
-            Utils.ChangeShaderEmission(target, new Color(0.90f, 0.86f, 0.61f, 1f));
+            baiLight.SetActive(false);
+            heiLight.SetActive(true);
         }
     }
 }
