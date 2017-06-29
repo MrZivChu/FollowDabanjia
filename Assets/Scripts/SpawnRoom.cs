9dd7bfb0f0f74b7fd8f0005df6a4f1452b677817
zhuwenhai <454719889@qq.com>
2017-06-29 10:36:01 +0800

    add

---
+++ Assets/Scripts/SpawnRoom.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnRoom : MonoBehaviour
+{
+    public static string roomPrefabPath = string.Empty;
+    private void Awake()
+    {
+        if (!string.IsNullOrEmpty(roomPrefabPath))
+        {
+            GameObject room = Instantiate(Resources.Load<GameObject>(roomPrefabPath)) as GameObject;
+            room.transform.position = Vector3.zero;
+
+            string playPath = roomPrefabPath;
+            int endIndex = playPath.LastIndexOf("/");
+            playPath = roomPrefabPath.Substring(0, endIndex);
+            GameObject player = Instantiate(Resources.Load(playPath + "/FPSController")) as GameObject;
+        }
+    }
+}
